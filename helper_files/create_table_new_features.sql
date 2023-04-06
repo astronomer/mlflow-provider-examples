aad3eb2640292a9fbea2a4fdfebed4eeacb2e0ef
Faisal <fhoda@users.noreply.github.com>
2023-04-06 14:34:02 -0500

    Update create_table_new_features.sql

--- helper_files/create_table_new_features.sql
+++ helper_files/create_table_new_features.sql
@@ -1,3 +1,18 @@
+-- SEQUENCE: public.new_features_feature_id_seq
+
+-- DROP SEQUENCE IF EXISTS public.new_features_feature_id_seq;
+
+CREATE SEQUENCE IF NOT EXISTS public.new_features_feature_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+ALTER SEQUENCE public.new_features_feature_id_seq
+    OWNER TO postgres;
+
+
 -- Table: public.new_features
 
 -- DROP TABLE IF EXISTS public.new_features;
